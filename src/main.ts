1467a6d74c0b8471b0f4a3da35ba01e5e236bc8a
Carlo Costa <carlo.costa92@gmail.com>
2025-08-04 16:35:09 +0200

    feat:solved bonus 2

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,12 @@ type Person = {
 // 📌 Milestone 2
 
 
-type Nationality = "American" | "British" | "Australian" | "Israeli-American" | "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean" | "Chinese";
+type NationalityActresses = "American" | "British" | "Australian" | "Israeli-American" | "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean" | "Chinese";
 
 type Actress = Person & {
   most_famous_movies: [string, string, string];
   awards: string;
-  nationality: Nationality
+  nationality: NationalityActresses
 }
 
 
@@ -145,7 +145,118 @@ function updateActress(actress: Actress, updates: Partial<Actress>): Actress {
   }
 }
 
+//   🎯 BONUS 2
 
+type ActorNationalities =
+  | NationalityActresses
+  | "Scottish"
+  | "New Zealand"
+  | "Hong Kong"
+  | "German"
+  | "Canadian"
+  | "Irish"
+
+type Actor = Person & {
+  known_for: [string, string, string];
+  awards: [string] | [string, string]
+  nationality: ActorNationalities
+}
+
+function isActor(data: unknown): data is Actor {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    "id" in data && typeof data.id === "number" &&
+    "name" in data && typeof data.name === "string" &&
+    "birth_year" in data && typeof data.birth_year === "number" &&
+    ("death_year" in data ? typeof (data as any).death_year === "number" : true) &&
+    "biography" in data && typeof data.biography === "string" &&
+    "image" in data && typeof data.image === "string" &&
+    "known_for" in data && data.known_for instanceof Array && data.known_for.length === 3 && data.known_for.every(m => typeof m === "string") &&
+    "awards" in data && data.awards instanceof Array && (data.awards.length === 1 || data.awards.length === 2) && data.awards.every(m => typeof m === "string") &&
+    "nationality" in data && typeof data.nationality === "string"
+  )
+}
+
+//getActor
+async function getActor(id: number): Promise<Actor | null> {
+  const endpoint = `http://localhost:3333/actors/${id}`
+
+  try {
+    const response = await fetch(endpoint);
+    const data: unknown = await response.json();
+    if (!isActor(data)) {
+      throw new Error("Formato dati non valido")
+    }
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Errore durante recuper attrice", error)
+    } else {
+      console.error("errore sconosciuto", error)
+    }
+    return null;
+  }
+}
+
+getActor(1)
+  .then(actress => {
+    console.log(actress)
+  })
+
+//getAllActors
+
+async function getAllActor(): Promise<Actor[]> {
+  const endpoint = `http://localhost:3333/actors`
+
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error(`errore http ${response.status}: ${response.statusText}`)
+    }
+    const data: unknown = await response.json();
+
+    if (!(data instanceof Array)) {
+      throw new Error("Formato non valido")
+    }
+
+    const actorValide: Actor[] = data.filter(isActor)
+    return actorValide;
+
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Errore durante recuper attrici", error)
+    } else {
+      console.error("errore sconosciuto", error)
+    }
+    return [];
+  }
+}
+
+getAllActor()
+  .then(Actor => {
+    console.log(Actor)
+  })
+
+//updateActor
+
+function updateActor(actor: Actor, updates: Partial<Actor>): Actor {
+  return {
+    ...actor,
+    ...updates,
+    id: actor.id,
+    name: actor.name
+  }
+}
+
+//createActor
+
+function createActor(data: Omit<Actor, "id">): Actor {
+  return {
+    ...data,
+    id: Math.floor(Math.random() * 10000)
+  }
+}
 
 
 
